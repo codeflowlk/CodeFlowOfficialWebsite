f88fca8b20031a58d0871ae42139f6c185af5a50
CodeFlow <realrajapaksha@gmail.com>
2020-11-17 23:11:21 +0530

    Slide Banner Creating
    Creating Side Banner using Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import Navbar from './Componant/NavBar';
+import React from "react";
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
-        {/*<Switch>*/}
-            {/*<Route path="/" exact component={FrontBody}/>*/}
-            {/*<Route path="/index" component={FrontBody}/>*/}
-            {/*<Route path="/signin" component={signin}/>*/}
-            {/*<Route path="/books" component={Books}/>*/}
-
-            {/*<Route component={Page404Error}/>*/}
-        {/*</Switch>*/}
 
     </div>
   );
--- frontend/src/Componant/NavBar.jsx
+++ frontend/src/Componant/NavBar.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../CSS/NavBar.css";
 import logo from "../Images/logoo.png";
+import SlideBanner from './SlideBanner';
 
-const NavBar = () => {
-    return (
-        <div>
-            <ul className="nav justify-content-center bg-dark">
-                <div className="justify-content-center">
-                    <li>
-                        <h1 className="head"><img className="rotateImage" src={logo} /> CodeFlow</h1>
-                    </li>
-                    <h4 className="tagline">DIGITAL SOLUTIONS AT ITS FINEST </h4>
-                </div>
-            </ul>
-        </div>
-    );
-};
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <ul className="nav justify-content-center bg-dark">
+                    <div className="justify-content-center">
+                        <li>
+                            <h1 className="head"><img className="rotateImage" src={logo} /> CodeFlow</h1>
+                        </li>
+                        <h4 className="tagline">DIGITAL SOLUTIONS AT ITS FINEST </h4>
+                    </div>
+                </ul>
+                <SlideBanner/>
+            </div>
+        );
+    }
+}
 
 export default NavBar;
