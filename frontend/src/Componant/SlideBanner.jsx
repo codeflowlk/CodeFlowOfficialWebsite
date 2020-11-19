66221bc88b2bc286205bebba87cbe9db416c90da
buminduyasith <bumiduyasith@gmail.com>
2020-11-19 15:19:47 +0530

    react carousel issue solved

--- frontend/src/Componant/SlideBanner.jsx
+++ frontend/src/Componant/SlideBanner.jsx
@@ -2,31 +2,41 @@ import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import bannerImage1 from "../Images/banner1.png";
 import bannerImage2 from "../Images/banner2.png";
+import bannerImage3 from "../Images/banner3.png";
+import {Jumbotron , Button ,Carousel} from 'react-bootstrap'
+import "../CSS/SlideBanner.css";
 
 class SlideBanner extends Component {
     render() {
         return (
-            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img className="d-block w-100" src={bannerImage1} alt="First slide"/>
-                    </div>
-                    <div className="carousel-item">
-                        <img className="d-block w-100" src={bannerImage2} alt="Second slide"/>
-                    </div>
-                    <div className="carousel-item">
-                        <img className="d-block w-100" src={bannerImage1} alt="Third slide"/>
-                    </div>
-                </div>
-                <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a>
-            </div>
+            <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={bannerImage1}
+                alt="First slide"
+              />
+             
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src= {bannerImage2}
+                alt="Third slide"
+              />
+          
+             
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={bannerImage3}
+                alt="Third slide"
+              />
+          
+            
+            </Carousel.Item>
+          </Carousel>
         );
     }
 }
